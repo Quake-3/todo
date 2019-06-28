cc4c3d6c84b9ced0ecbe750966b9c79459805dfd
Quake-3 <52196778+Quake-3@users.noreply.github.com>
2019-06-28 16:21:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+
+document.getElementsByClassName('addBtn').onclick = function() {
+  if(document.getElementsByTagName('li')) {
+    localStorage.setItem('li', "document.createElement('li')");
+  } else {
+    localStorage.setItem('li', "false");
+  }
+}
+if (localStorage.getItem('li') == "document.createElement('li')") {
+  document.createElement("li");
+}
+
+
+
+
+
+
+
+
+}
+
+class Todo {
+	constructor(text,isComplete = false){
+		this.text = text;
+		this.complete = !isComplete;
+	}
+	add(){
+
+	}
+	remove(){
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
